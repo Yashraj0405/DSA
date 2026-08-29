c7b73c96d049397b69f1145400e3edc13719f7d0
Yashraj <91713390+Yashraj0405@users.noreply.github.com>
2026-08-29 19:13:21 +0530

    Time: 3 ms (100%), Space: 175.4 MB (70.6%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -5,10 +5,10 @@ class Solution {
         for(int i = 0 ; i < nums.length; i++){
             if(nums[i]>= 0){
                 ans[pos] = nums[i];
-                pos = pos+2;
+                pos += 2;
             }else{
                 ans[neg] = nums[i];
-                neg = neg+2;
+                neg += 2;
             }
         }
         return ans;
