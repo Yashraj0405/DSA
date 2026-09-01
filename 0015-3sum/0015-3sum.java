c4433fd53150f1570dce5a85a36951238f380af2
Yashraj <91713390+Yashraj0405@users.noreply.github.com>
2026-09-01 23:31:03 +0530

    Time: 35 ms (42.41%), Space: 59.3 MB (45.28%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         //Better
-        Set<List<Integer>> result = new HashSet<>();
+        // Set<List<Integer>> result = new HashSet<>();
+
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     Set<Integer> hashSet = new HashSet<>();
+        //     for(int j = i + 1 ; j < nums.length ; j++){
+        //         int third = -(nums[i]+nums[j]);
+
+        //         if(hashSet.contains(third)){
+        //             List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
+        //             Collections.sort(temp);
+        //             result.add(temp);
+        //         }
+        //         hashSet.add(nums[j]);
+        //     }
+        // }
+
+        // return new ArrayList<>(result);
+
+        //Optimal
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
         for(int i = 0 ; i < nums.length ; i++){
-            Set<Integer> hashSet = new HashSet<>();
-            for(int j = i + 1 ; j < nums.length ; j++){
-                int third = -(nums[i]+nums[j]);
+                if(i > 0 && nums[i] == nums[i -1]) continue;
 
-                if(hashSet.contains(third)){
-                    List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
-                    Collections.sort(temp);
-                    result.add(temp);
+                int left = i + 1 , right = nums.length - 1;
+                while(left<right){
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if(sum == 0){
+                        ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                        left++;
+                        right--;
+
+                        while(left<right && nums[left-1] == nums[left]) left++;
+                        while(left<right && nums[right+1] == nums[right]) right--;
+                    }
+                    else if(sum < 0) left++;
+                    else right--;
                 }
-                hashSet.add(nums[j]);
-            }
         }
 
-        return new ArrayList<>(result);
+        return ans;
+
     }
 }
