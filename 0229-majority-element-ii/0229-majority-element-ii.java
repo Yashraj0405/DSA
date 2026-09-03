92fa725afedf47514509278c6fa7a120301987b2
Yashraj <91713390+Yashraj0405@users.noreply.github.com>
2026-09-03 22:25:56 +0530

    Time: 3 ms (73.12%), Space: 52.8 MB (22.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -18,21 +18,54 @@ class Solution {
 
 
         //Better
-        int n = nums.length;
+        // int n = nums.length;
+        // List<Integer> result = new ArrayList<>();
+        // Map<Integer, Integer> mpp = new HashMap<>();
+        // int mini = n / 3 + 1;
+
+        // for (int i = 0; i < n; i++) {
+        //     mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+
+        //     // Add to result only when the count just reaches mini
+        //     if (mpp.get(nums[i]) == mini) {
+        //         result.add(nums[i]);
+        //     }
+
+        //     if (result.size() == 2) break;
+        // }
+
+        // return result;
+
+        //Optimal
         List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> mpp = new HashMap<>();
-        int mini = n / 3 + 1;
+        int counter1 = 0 , counter2 = 0 , element1 = Integer.MIN_VALUE , element2 = Integer.MIN_VALUE, 
+        maxCount1 = 0 ,maxCount2 = 0, mini = nums.length / 3 + 1;;
 
-        for (int i = 0; i < n; i++) {
-            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+        for(int i = 0 ; i < nums.length ; i++){
 
-            // Add to result only when the count just reaches mini
-            if (mpp.get(nums[i]) == mini) {
-                result.add(nums[i]);
+            if(counter1 == 0 && nums[i] != element2){
+                element1 = nums[i];
+                counter1 = 1;
             }
-
-            if (result.size() == 2) break;
+            else if(counter2 == 0 & nums[i] != element1){
+                element2 = nums[i];
+                counter2 = 1;
+            }
+            else if(nums[i] == element1) counter1++;
+            else if(nums[i] == element2) counter2++;
+            else {
+                counter1--;
+                counter2--;
+            }
         }
+
+        for(int num : nums){
+            if(num == element1)maxCount1++;
+            if(num == element2)maxCount2++;
+        }
+
+        if(maxCount1 >= mini) result.add(element1);
+        if(maxCount2 >= mini && element1 != element2) result.add(element2);
 
         return result;
 
