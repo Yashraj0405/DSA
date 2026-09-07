db9e087764c1cf1f6fde2650d250d8ee59fa8bbc
Yashraj <91713390+Yashraj0405@users.noreply.github.com>
2026-09-07 21:39:32 +0530

    Time: 33 ms (71.21%), Space: 59.2 MB (59.69%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -21,28 +21,28 @@ class Solution {
 
         //Optimal
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-
+        List<List<Integer>> result = new ArrayList<>();
         for(int i = 0 ; i < nums.length ; i++){
-                if(i > 0 && nums[i] == nums[i -1]) continue;
+            if( i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1, right = nums.length - 1;
 
-                int left = i + 1 , right = nums.length - 1;
-                while(left<right){
-                    int sum = nums[i] + nums[left] + nums[right];
-                    if(sum == 0){
-                        ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                        left++;
-                        right--;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
 
-                        while(left<right && nums[left-1] == nums[left]) left++;
-                        while(left<right && nums[right+1] == nums[right]) right--;
-                    }
-                    else if(sum < 0) left++;
-                    else right--;
+                    while(left < right && nums[left] == nums[left-1])left++;
+                    while(left < right && nums[right] == nums[right+1])right--;
                 }
+                else if(sum < 0 )left ++;
+                else right--;
+            }
+
         }
 
-        return ans;
+        return result;
 
     }
 }
