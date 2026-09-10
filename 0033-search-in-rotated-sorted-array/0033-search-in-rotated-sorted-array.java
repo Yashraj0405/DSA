a4e758162b2d14004c8f65e096d03c2405120e38
Yashraj <91713390+Yashraj0405@users.noreply.github.com>
2026-09-10 22:09:38 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,14 +8,14 @@ class Solution {
 
             // If left part is sorted
             if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid])
+                if (nums[left] <= target && target <= nums[mid])
                     right = mid - 1;
                 else
                     left = mid + 1;
             } 
             // Else, right part is sorted
             else {
-                if (nums[mid] < target && target <= nums[right])
+                if (nums[mid] <= target && target <= nums[right])
                     left = mid + 1;
                 else
                     right = mid - 1;
